f29ff18847e28e78dce577e9ce3ac370e57affad
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-06-03 02:32:21 +0600

    gel all user

--- index.js
+++ index.js
@@ -32,6 +32,11 @@ async function run() {
     const addToCartCollection = client.db("heavenlyFeast").collection("addToCart");
 
     // user api 
+    app.get('/users', async(req, res) =>{
+      const result = await usersCollection.find().toArray()
+      res.send(result)
+    })
+
     app.post('/users', async(req, res) => {
       const user = req.body
       const query = {email: user.email}
