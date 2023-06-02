e06e5cd07b4bf8abd388f28643008ade1e63e4a8
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-06-02 23:41:18 +0600

    dalete data frome server

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const cors = require('cors');
-const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
-const port = process.env.PORT || 5000
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xvcivem.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -16,7 +16,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -24,30 +24,73 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const heavenlyFeastMenuCollection = client.db("heavenlyFeast").collection("menu");
+    const usersCollection = client.db("heavenlyFeast").collection("users");
+    const heavenlyFeastMenuCollection = client
+      .db("heavenlyFeast")
+      .collection("menu");
     const reviewCollection = client.db("heavenlyFeast").collection("review");
     const addToCartCollection = client.db("heavenlyFeast").collection("addToCart");
 
-
-    app.get('/menu', async(req, res) => {
-        const result = await heavenlyFeastMenuCollection.find().toArray()
-        res.send(result);
-    })
-
-    app.get('/review', async(req, res) => {
-        const result = await reviewCollection.find().toArray()
-        res.send(result)
-    })
-    app.post('/carts', async(req, res) => {
-      const item = req.body
-      console.log(item);
-      const result = await addToCartCollection.insertOne(item)
+    // user api 
+    app.post('/users', async(req, res) => {
+      const user = req.body
+      const result = await usersCollection.insertOne(user)
       res.send(result)
     })
 
+
+    //menu api
+    app.get("/menu", async (req, res) => {
+      const result = await heavenlyFeastMenuCollection.find().toArray();
+      res.send(result);
+    });
+
+    // reviewCollection
+    app.get("/review", async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
+    });
+
+    // add to cart api 
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        res.send([]);
+      }
+      const query = { email: email };
+      const result = await addToCartCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/carts", async (req, res) => {
+      const item = req.body;
+      const query = { id: item.id };
+      try {
+        const findItem = await addToCartCollection.find(query).toArray();
+        if (findItem.length === 0) {
+          const result = await addToCartCollection.insertOne(item);
+          res.send(result);
+        } else {
+          res.send([]);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    });
+
+    // delete add to cart 
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await addToCartCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -55,11 +98,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('Heavenly Feast restaurant')
-})
+app.get("/", (req, res) => {
+  res.send("Heavenly Feast restaurant");
+});
 
 app.listen(port, () => {
-  console.log(`Heavenly Feast restaurant is running on port ${port}`)
-})
+  console.log(`Heavenly Feast restaurant is running on port ${port}`);
+});
