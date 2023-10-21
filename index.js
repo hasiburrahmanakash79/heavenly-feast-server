5e4fa9e956c67e163676583bee1cc2919b4f6410
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-10-21 13:58:28 +0600

    make ssl payment

--- index.js
+++ index.js
@@ -79,6 +79,53 @@ async function run() {
       next();
     };
 
+    const tran_id = new ObjectId().toString()
+    app.post('/order', async(req, res) => {
+      console.log(req.body);
+      const item = await addToCartCollection.findOne({
+        _id: new ObjectId(req.body.itemsID)
+      })
+      console.log(item, "------------87");
+
+      const data = {
+        total_amount: item?.price,
+        currency: req.body?.currency,
+        tran_id: 'REF123', // use unique tran_id for each api call
+        success_url: 'http://localhost:3030/success',
+        fail_url: 'http://localhost:3030/fail',
+        cancel_url: 'http://localhost:3030/cancel',
+        ipn_url: 'http://localhost:3030/ipn',
+        shipping_method: 'Courier',
+        product_name: item?.name,
+        product_category: 'Electronic',
+        product_profile: item?.image,
+        cus_name: req.body?.name,
+        cus_email: req.body?.email,
+        cus_add1: req.body?.location,
+        cus_add2: 'Dhaka',
+        cus_city: 'Dhaka',
+        cus_state: 'Dhaka',
+        cus_postcode: '1000',
+        cus_country: 'Bangladesh',
+        cus_phone: req.body?.phone,
+        cus_fax: '01711111111',
+        ship_name: 'Customer Name',
+        ship_add1: 'Dhaka',
+        ship_add2: 'Dhaka',
+        ship_city: 'Dhaka',
+        ship_state: 'Dhaka',
+        ship_postcode: 1000,
+        ship_country: 'Bangladesh',
+    };
+    // const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
+    // sslcz.init(data).then(apiResponse => {
+    //     // Redirect the user to payment gateway
+    //     let GatewayPageURL = apiResponse.GatewayPageURL
+    //     res.redirect(GatewayPageURL)
+    //     console.log('Redirecting to: ', GatewayPageURL)
+    // });
+    })
+
     // user api
     app.get("/users", verifyJWT, verifyAdmin, async (req, res) => {
       const result = await usersCollection.find().toArray();
